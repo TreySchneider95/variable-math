0f81c98e94159dbdc6ff1998933f23697f0d51b2
TreySchneider95 <tschneid95@gmail.com>
2022-09-13 12:03:29 -0600

    small change to capitol

--- remainder.js
+++ remainder.js
@@ -1,4 +1,4 @@
 let num1 = 7
 let num2 = 3
 
-console.log(`the remainder of ${num1} divided by ${num2} is ${num1 % num2}`)
+console.log(`The remainder of ${num1} divided by ${num2} is ${num1 % num2}`)
